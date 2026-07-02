d3b3c0b4d8189c698e580e7a68a20ac83f2e38cd
ponjun4869 <ponjun0529@gmail.com>
2026-07-02 17:24:51 +0900

    タスク新規登録機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,8 +8,27 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
 
   def edit
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
+
