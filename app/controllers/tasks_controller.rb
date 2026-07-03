59553a7fc001cccab7e59c5e27775ffe2f33bbea
ponjun4869 <ponjun0529@gmail.com>
2026-07-03 19:13:17 +0900

    タスク編集機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,8 +21,18 @@ class TasksController < ApplicationController
     end
   end
   
-
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to  tasks_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
