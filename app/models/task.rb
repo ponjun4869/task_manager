db33f4610b16c9745e4d614738e3450ea29f4125
ponjun4869 <ponjun0529@gmail.com>
2026-07-04 23:44:01 +0900

    アカウント認証機能の実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 500 }
 end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
