9a020b4b9611c133a2ccea4347af27ebeef79172
ponjun4869 <ponjun0529@gmail.com>
2026-07-05 17:10:54 +0900

    ステータス管理機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :status)
   end
 end
